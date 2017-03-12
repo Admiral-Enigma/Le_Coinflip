f2e5f433830ef694d67385e681e5499f1cb8f235
Alexander <mrrandomgamerdkhd@gmail.com>
2017-03-12 20:40:26 +0100

    Made a little script to remove the log files

---
+++ clean.js
@@ -0,0 +1,20 @@
+var fs = require('fs')
+console.log('run');
+function rmDir(dirPath, removeSelf) {
+  if (removeSelf === undefined)
+    removeSelf = true;
+  try { var files = fs.readdirSync(dirPath); }
+  catch(e) { return; }
+  if (files.length > 0)
+    for (var i = 0; i < files.length; i++) {
+      var filePath = dirPath + '/' + files[i];
+      if (fs.statSync(filePath).isFile())
+        fs.unlinkSync(filePath);
+      else
+        rmDir(filePath);
+    }
+  if (removeSelf)
+    fs.rmdirSync(dirPath);
+};
+
+rmDir('logs/', false)
