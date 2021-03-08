282c86ef73931417f3cd974a9ead60388058110d
Alexander <MrRandomgamerdkHD@gmail.com>
2021-03-08 17:27:50 +0100

    Made the coin comeback, yay

--- www/js/ui.js
+++ www/js/ui.js
@@ -1,28 +1,34 @@
 var ui = {
   getNameInput: function () {
     var name = ''
-    swal({
-      title: 'Welcome to Le Coinflip!',
-      text: 'Input a name to begin betting!',
-      type: 'input',
-      showCancelButton: false,
-      closeOnConfirm: false,
-      animation: "slide-from-top",
-      inputPlaceholder: "Joe"
-    }, function (inputValue) {
-      if (inputValue === false) return false;
+    swal(
+      {
+        title: 'Welcome to Le Coinflip!',
+        text: 'Input a name to begin betting!',
+        type: 'input',
+        showCancelButton: false,
+        closeOnConfirm: false,
+        animation: 'slide-from-top',
+        inputPlaceholder: 'Joe',
+      },
+      function (inputValue) {
+        if (inputValue === false) return false
 
-      if (inputValue === "") {
-        swal.showInputError("You need to write something!");
-        return false
+        if (inputValue === '') {
+          swal.showInputError('You need to write something!')
+          return false
+        }
+
+        name = inputValue
+        user.setupUser(name)
+        swal('Welcome ' + name + '!', 'You can now start betting!')
       }
-
-      name = inputValue
-      user.setupUser(name)
-      swal('Welcome '+name+'!', 'You can now start betting!')
-    })
+    )
   },
   wonPopup: function (amount, username) {
-    swal('･｡ﾟ[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]｡ﾟ.*', 'Congrats '+username+' you\'ve won '+amount+'bits!')
-  }
+    swal(
+      '･｡ﾟ[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]｡ﾟ.*',
+      'Congrats ' + username + " you've won " + amount + ' bits!'
+    )
+  },
 }
